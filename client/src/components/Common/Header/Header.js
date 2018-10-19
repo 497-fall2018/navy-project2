ca6a39e0d2f841cc667507c07044c254eeb75bba
yulheekim <yulkim2019@u.northwestern.edu>
2018-10-19 01:56:40 -0500

    school selection added

--- client/src/components/Common/Header/Header.js
+++ client/src/components/Common/Header/Header.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
-
+import { connect } from 'react-redux';
+import { MenuItem, Select } from '@material-ui/core';
 import './styles.css';
+import {
+    handle_school_change,
+} from '../../../ducks/post';
 
-
-class Header extends Component {
-
+class HeaderComponent extends Component {
+    handleSchoolChange = (event) => {
+        this.props.handle_school_change(event.target.value);
+    }
     render() {
         const landingUrl = "lostnfound.mmorderell.com";
         return (
@@ -12,6 +17,21 @@ class Header extends Component {
             <span>Lost & Found</span>
 
             <div className="headerLinks">
+                <Select
+                    value={this.props.school}
+                    onChange={this.handleSchoolChange}
+                    inputProps={{
+                      name: 'School',
+                    }}
+                    style={{"color":"white", "fontWeight":"600"}}
+                >
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={"Northwestern University"}>Northwestern University</MenuItem>
+                    <MenuItem value={"University of Chicago"}>University of Chicago</MenuItem>
+                    <MenuItem value={"University of Illinois at Urbana-Champagne"}>University of Illinois at Urbana-Champagne</MenuItem>
+                </Select>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 <a href={landingUrl} className="topRightLink">
                     Lost
                 </a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -24,5 +44,19 @@ class Header extends Component {
 }
 
 export {
-    Header
+    HeaderComponent
 };
+
+
+const mapStateToProps = (state, ownProps) => {
+    const { post } = state;
+    const { school } = post;
+    return {
+        ...ownProps,
+        school
+    };
+};
+
+export const Header = connect(mapStateToProps, {
+    handle_school_change,
+})(HeaderComponent);
