4f0daad93de3f6dbe22772a4c8716195e99da3bc
Matt Moderwell <mmoderwell@gmail.com>
2018-11-02 01:20:07 +0000

    Updated adding lost item to have all fields

--- backend/routes.js
+++ backend/routes.js
@@ -7,6 +7,7 @@ module.exports = (app) => {
 		res.json({ "hello": "world" });
 	});
 	//route to add new lost item to DB
+	//need to add image upload at some point
 	app.post('/api/lost', (req, res) => {
 		const item = new lostItem();
 		//get lost item info from the frontend
@@ -18,11 +19,14 @@ module.exports = (app) => {
 				error: 'You must provide a name, caption, and image.'
 			});
 		}
-		//just the important ones for first demo
+		//set all of the fields we got from the fontend
 		item.name = name;
 		item.location = location;
+		item.email = email;
 		item.description = description;
+		item.school = school;
 		item.reward = reward;
+		item.password = password;
 
 		item.save(err => {
 			if (err) return res.json({ success: false, error: err });
