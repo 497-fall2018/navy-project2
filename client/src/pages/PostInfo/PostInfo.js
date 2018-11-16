9d365182faaa1ffddb7c4f8cf7e43479ba921180
yinglanli <yinglanli0307@gmail.com>
2018-11-15 20:44:31 -0600

    update post

--- client/src/pages/PostInfo/PostInfo.js
+++ client/src/pages/PostInfo/PostInfo.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { connect } from 'react-redux';
 import {
     toggle_modal,
+    toggle_update_modal,
     handle_delete_post,
-    handle_form_change
+    handle_form_change,
+    handle_update_post
 } from '../../ducks/post';
 import './styles.css';
 import { Header } from '../../components';
 import { Typography, Button, TextField } from '@material-ui/core';
-import { Delete } from '@material-ui/icons';
+import { Delete, Edit } from '@material-ui/icons';
 import Modal from 'react-responsive-modal';
+import { Redirect } from "react-router-dom";
 
 class PostInfoComponent extends React.Component {
     handleFormChange = (name) => (event) => {
@@ -18,14 +21,23 @@ class PostInfoComponent extends React.Component {
     toggleModal = () => {
         this.props.toggle_modal();
     }
-    handleDeletePost = (formType, id) => {
-        this.props.handle_delete_post(formType, id, this.props.password);
+    toggleUpdateModal = () => {
+        this.props.toggle_update_modal();
+    }
+    handleDeletePost = (id) => {
+        this.props.handle_delete_post(this.props.lorf, id, this.props.password);
+    }
+    handleUpdatePost = (item) => {
+        this.props.handle_update_post(this.props.lorf, item, this.props.password);
     }
 
     render() {
         const item = this.props.lorf==="lost" ? this.props.lost.find(item => item._id===this.props.match.params.id) :  this.props.found.find(item => item._id===this.props.match.params.id)
-        console.log(item);
-        console.log(this.props.error_message);
+
+        if (this.props.redirect) {
+            return (<Redirect to='/postform' />);
+        }
+
         return (
             <div>
                 <Header />
@@ -40,6 +52,30 @@ class PostInfoComponent extends React.Component {
                     </div>)}
                 <Typography>{item.reward}</Typography>
                   
+                <Button onClick={this.toggleUpdateModal}>
+                    <Edit />
+                </Button>
+                <Modal
+                  open={this.props.update_modal_open}
+                  onClose={this.toggleUpdateModal}
+                  center
+                  classNames={{ overlay: 'custom-overlay', modal: 'custom-modal' }}
+                >
+                    <h2 style={{fontFamily:'monospace'}}>Please enter the password</h2>
+                    <TextField required
+                      label="Password"
+                      margin="normal"
+                      autoFocus={true}
+                      helperText={this.props.error_message}
+                      onChange={this.handleFormChange("password")}
+                    />
+                    <br/><br/><br/>
+                    <Button variant="contained" color="primary" onClick={() => {this.handleUpdatePost(item);}}>
+                        Next
+                    </Button>
+                    <Button onClick={this.toggleUpdateModal} style={{color: '#4054AC'}}>Cancel</Button>
+                </Modal>
+
                 <Button onClick={this.toggleModal}>
                     <Delete />
                 </Button>
@@ -49,20 +85,21 @@ class PostInfoComponent extends React.Component {
                   center
                   classNames={{ overlay: 'custom-overlay', modal: 'custom-modal' }}
                 >
-                    <h2 style={{fontFamily:'monospace'}}>Delete Post</h2>
+                    <h2 style={{fontFamily:'monospace'}}>Please enter the password</h2>
                     <TextField required
                       label="Password"
                       margin="normal"
                       autoFocus={true}
+                      helperText={this.props.error_message}
                       onChange={this.handleFormChange("password")}
                     />
-                    <p>{this.props.error_message}</p>
                     <br/><br/><br/>
-                    <Button variant="contained" color="primary" onClick={() => {this.handleDeletePost(this.props.lorf, item._id);}}>
+                    <Button variant="contained" color="primary" onClick={() => {this.handleDeletePost(item._id);}}>
                         Delete
                     </Button>
                     <Button onClick={this.toggleModal} style={{color: '#4054AC'}}>Cancel</Button>
                 </Modal>
+                
             </div>
         );
     }
@@ -72,19 +109,23 @@ export {PostInfoComponent};
 
 const mapStateToProps = (state, ownProps) => {
     const { post } = state;
-    const { lost, found, modal_open, error_message, password } = post;
+    const { lost, found, modal_open, update_modal_open, error_message, password, redirect } = post;
     return {
         ...ownProps,
         lost,
         found,
         modal_open,
+        update_modal_open,
         error_message,
-        password
+        password,
+        redirect
     };
 };
 
 export const PostInfo=connect(mapStateToProps,{
     toggle_modal,
+    toggle_update_modal,
     handle_delete_post,
-    handle_form_change
+    handle_form_change,
+    handle_update_post
 })(PostInfoComponent);
